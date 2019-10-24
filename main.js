db78c8a71cbc8320fca5bac9bffafbaf7e0faca2
Yaindrop <yuan_dian@live.com>
2019-10-23 21:25:06 -0700

    style change

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
-var updateSize = function() {
+var updateSize = function () {
     var w = window,
-    d = document,
-    e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    x = w.innerWidth || e.clientWidth || g.clientWidth;
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth;
     var title = d.getElementsByTagName("title")[0].innerHTML;
     var head = d.getElementById("head");
     if (x <= 700) {
         if (title.length > 15) {
-            newSize = parseInt(150 / title.length * 10) / 10 + "vw";
-            console.log(newSize);
-            head.style.fontSize = newSize;
-        }
-    } else if (x <= 1400) {
-        if (title.length > 24) {
-            newSize = parseInt(144 / title.length * 10) / 10 + "vw";
-            head.style.fontSize = newSize;
+            head.style.fontSize = parseInt(150 / title.length) + "vw";
         }
     } else {
         head.style.fontSize = "3rem";
     }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    console.log("here");
+document.addEventListener("DOMContentLoaded", function (event) {
     updateSize();
 });
 
